eca221221a61f2e23c763825415eb726f8ad703b
positlabs <josh.beckwith@gmail.com>
2022-04-05 16:10:26 -0400

    good enough

--- src/components/lpl-landing/lpl-landing.tsx
+++ src/components/lpl-landing/lpl-landing.tsx
@@ -19,9 +19,11 @@ export class LplLanding {
 
   onResize() {
     // apply correct aspect ratio to youtube iframe
-    const ytIframe = this.$('iframe')
-    Object.assign(ytIframe.style, {
-      height: ytIframe.offsetWidth * 9 / 16 + 'px'
+    const iframes = this.$$('iframe')
+    iframes.forEach(iframe => {
+      Object.assign(iframe.style, {
+        height: iframe.offsetWidth * 9 / 16 + 'px'
+      })
     })
   }
 
@@ -51,10 +53,8 @@ export class LplLanding {
     const proString = version === 4 ? 'pro-' : ''
     if (isMac) {
       url = `https://storage.googleapis.com/lightpaintlive.appspot.com/builds/mac/lpl-mercury-${proString}${data.version}.dmg`
-      // url = 'https://s3-us-west-2.amazonaws.com/lightpaintlive-mercury/'+data.version+'/lpl-mercury-'+data.version+'.dmg'
     } else if (isWin) {
       url = `https://storage.googleapis.com/lightpaintlive.appspot.com/builds/win/lpl-mercury-${proString}${data.version}%20Setup.exe`
-      // url = 'https://s3-us-west-2.amazonaws.com/lightpaintlive-mercury/'+data.version+'/lpl-mercury-'+data.version+'+Setup.exe'
     }
     if (url) {
       window.location = url
@@ -77,7 +77,8 @@ export class LplLanding {
                 <div class="icon"></div><a href="#">Download V3</a>
               </div>
             </div>
-            <iframe id="ytplayer" src="https://www.youtube.com/embed/jzrUNDacs1A?list=PLAZp2Vi7Gfsouo7T5XA39-M_swalzOs9C&listType=playlist&modestbranding=1" frameborder="0" allowFullScreen></iframe>
+            <iframe class="ytplayer" src="https://www.youtube.com/embed/jzrUNDacs1A?list=PLAZp2Vi7Gfsouo7T5XA39-M_swalzOs9C&listType=playlist&modestbranding=1" frameborder="0" allowFullScreen></iframe>
+            <iframe class="ytplayer" src="https://www.youtube.com/embed/-zE527Nt460?list=PLAZp2Vi7Gfsouo7T5XA39-M_swalzOs9C&amp;listType=playlist&amp;modestbranding=1" frameborder="0" allowFullScreen></iframe>
           </section>
           <section id="faq">
             <h1>Frequently asked questions</h1>
@@ -97,37 +98,33 @@ export class LplLanding {
               Use your webcam's driver software to control settings on the webcam, or use a universal webcam
               controller
               like <a class="shiny" href="https://itunes.apple.com/us/app/webcam-settings/id533696630?mt=12"
-                target="_blank">Webcam&nbsp;Settings</a> (Mac).
-              <a class="shiny" href="https://obsproject.com/">OBS</a> also has webcam controls on Windows.</p>
+                target="_blank">Webcam&nbsp;Settings</a> (Mac). <a class="shiny" href="https://obsproject.com/">OBS</a> also has webcam controls on Windows.</p>
 
             <h2>How can I force Mercury to use the dedicated GPU card?</h2>
+            
             <h3>Nvidia</h3>
             <p>Open the NVIDIA control panel. Right-click on an empty area on your desktop and select the ‘NVIDIA Control Panel’ option from the context menu.</p>
             <p>Go to the Desktop menu and enable the ‘Add “Run with graphics processor” to Context Menu’ option.</p>
             <p>Add a shortcut to Mercury to the desktop, then right-click and select “Run with graphics processor”.</p>
+
             <h3>AMD</h3>
             <p>Open the AMD Catalyst Control Center app by right-clicking on an empty area on your desktop. Select ‘Catalyst Control Center’ from the context menu. From the list of panels on the left, select ‘Power’. Under ‘Power’, select ‘Switchable application graphics settings’. Add Mercury to this list.</p>
-            <p>Read <a href='https://www.addictivetips.com/windows-tips/force-app-to-use-dedicated-gpu-windows/' target='_blank'>How To Force An App To Use The Dedicated GPU On Windows</a> for a more detailed explanation.</p>
+            <p>Read <a class="shiny" href='https://www.addictivetips.com/windows-tips/force-app-to-use-dedicated-gpu-windows/' target='_blank'>How To Force An App To Use The Dedicated GPU On Windows</a> for a more detailed explanation.</p>
 
           </section>
           <section id="story">
             <h1>story</h1>
             <p>LPL started as a simple prototype. It had two buttons and was very slow. It was discovered by Joerg
-              Miedza, a prominent member of the light-painting community. The app was very limited, but Joerg saw it
-              as the next step
-              in the evolution of light-painting. He wanted to know if it could be improved, so he emailed the
-              developer, Josh Beckwith.
+              Miedza, a prominent member of the light-painting community. The app was very limited, but Joerg saw it as the next step
+              in the evolution of light-painting. He wanted to know if it could be improved, so he emailed the developer, Josh Beckwith.
               Ever since then, Josh and Joerg have been exchanging ideas and improving LPL. </p>
             <div class="person">
               <h3>Joerg Miedza</h3><img src="assets/images/joerg.jpg" />
               <div class="content-block">
                 <p>Joerg Miedza has been creating movies and photos using special techniques since the late 90's.
-                  He is the co-founder of the german light-art project LAPP-PRO. In this project, the LAPP-Team
-                  developed a
-                  unique brand of light painting: LightArt Performance Photography, or LAPP for short. The skills
-                  Joerg acquired
-                  working with motion pictures allowed him to contribute significantly to the performance and
-                  artistic aspect of
+                  He is the co-founder of the german light-art project LAPP-PRO. In this project, the LAPP-Team developed a
+                  unique brand of light painting: LightArt Performance Photography, or LAPP for short. The skills Joerg acquired
+                  working with motion pictures allowed him to contribute significantly to the performance and artistic aspect of
                   LAPP photography.</p>
                 <p>His personal work is at <a class="shiny" href="http://miedza.de" target="_blank">miedza.de</a></p>
                 <p>There is also a book of the work: <a class="shiny" href="http://shop.oreilly.com/product/9781933952741.do"
@@ -139,8 +136,8 @@ export class LplLanding {
               <h3>Josh Beckwith</h3><img src="assets/images/josh.jpg" />
               <div class="content-block">
                 <p>Josh has always been obsessed with science and technology - and as a child, he dreamed of
-                  becoming an inventor. Now, he's a senior interactive developer at <a class="shiny" href="http://toolofna.com"
-                    target="_blank">Tool of North America.</a></p>
+                  becoming an inventor. Now, he's a senior interactive developer at <a class="shiny" href="https://popul-ar.com"
+                    target="_blank">PopulAR.</a></p>
                 <p>Email: <a class="shiny" href="mailto:josh@lightpaintlive.com" target="_blank">josh@lightpaintlive.com</a></p>
                 <p>Instagram: <a class="shiny" href="https://instagram.com/positlabs" target="_blank">@positlabs</a></p>
               </div>
@@ -155,20 +152,14 @@ export class LplLanding {
   $(selector) {
     return this.el.querySelector(selector) as HTMLElement
   }
+
+  $$(selector) {
+    return Array.from(this.el.querySelectorAll(selector)) as Array<HTMLElement>
+  }
 }
 /*
 
-
-  <div id="download-ui" class="ui">
-    <h1>Download Mercury ${this.pro ? 'Pro' : 'v3'}</h1>
-    <p>
-      <div class='dl-btn' data-type='mac'><img width='15' src='/assets/images/apple.svg'/> MAC INSTALLER</div>
-      <div class='dl-btn' data-type='win'><img width='12' src='/assets/images/windows.svg'/> WIN INSTALLER</div>
-    </p>
     <p>Your OS might try to block the installation. Here are instructions on how to get around that. <a class="shiny" href="http://kb.mit.edu/confluence/display/istcontrib/Allow+application+installations+and+temporarily+disable+Gatekeeper+in+OS+X+10.9+and+up" target="_blank">Mac</a><span>
       - </span><a class="shiny" href="https://www.windowscentral.com/how-fix-app-has-been-blocked-your-protection-windows-10" target="blank">Windows</a></p>
-  </div>
-
-
 
 */
